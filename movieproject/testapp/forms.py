79afff0085045333cb0b7c013c8f0c70b485d6f3
Dheeraj Kumar <dhiraj.bbcl@gmail.com>
2022-05-16 13:24:08 +0530

    initial commit

---
+++ movieproject/testapp/forms.py
@@ -0,0 +1,8 @@
+from dataclasses import field
+from django import forms
+from testapp.models import Movie
+
+class MovieForm(forms.ModelForm):
+    class Meta:
+        model = Movie
+        fields = '__all__'
